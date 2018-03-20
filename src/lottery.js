4991a12e469b6cfbd841a3eb83f30e52df8e5cfb
Arturo <iglesias294@icloud.com>
2018-03-20 10:22:41 +0800

    can get smart contract info via web3

---
+++ src/lottery.js
@@ -0,0 +1,65 @@
+import web3 from './web3';
+
+const address = '0x16533a169C410157BB465956809824D744Fa4629';
+const abi = [{
+  "constant": true,
+  "inputs": [],
+  "name": "manager",
+  "outputs": [{
+    "name": "",
+    "type": "address"
+  }],
+  "payable": false,
+  "stateMutability": "view",
+  "type": "function"
+}, {
+  "constant": false,
+  "inputs": [],
+  "name": "pickWinner",
+  "outputs": [],
+  "payable": false,
+  "stateMutability": "nonpayable",
+  "type": "function"
+}, {
+  "constant": true,
+  "inputs": [],
+  "name": "getPlayers",
+  "outputs": [{
+    "name": "",
+    "type": "address[]"
+  }],
+  "payable": false,
+  "stateMutability": "view",
+  "type": "function"
+}, {
+  "constant": false,
+  "inputs": [],
+  "name": "enter",
+  "outputs": [],
+  "payable": true,
+  "stateMutability": "payable",
+  "type": "function"
+}, {
+  "constant": true,
+  "inputs": [{
+    "name": "",
+    "type": "uint256"
+  }],
+  "name": "players",
+  "outputs": [{
+    "name": "",
+    "type": "address"
+  }],
+  "payable": false,
+  "stateMutability": "view",
+  "type": "function"
+}, {
+  "inputs": [],
+  "payable": false,
+  "stateMutability": "nonpayable",
+  "type": "constructor"
+}]
+
+
+export default new web3.eth.Contract(abi, address);
+
