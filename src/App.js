c3ab2213f7c956094942475fb87c01667fefb706
Arturo <iglesias294@icloud.com>
2018-03-20 11:09:16 +0800

    working lottery

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
   state = {
     manager: '',
     players: [],
-    balance: ''
+    balance: '',
+    value: '',
+    message: ''
   }
 
   async componentDidMount(){
@@ -18,11 +20,63 @@ class App extends Component {
 
     this.setState({ manager, players, balance });
   }
+
+  onClick = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: 'Waiting on transaction success...' })
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    this.setState({ message: 'A winner has been picked!' });
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: 'Waiting on transaction success...'})
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    })
+
+    this.setState({ message: 'You have been entered!'})
+
+  }
+
   render() {
     return (
       <div>
         <h2> Lottery Contract</h2>
         <p>This contract is managed by {this.state.manager}</p>
+        <p> Number of people entered: {this.state.players.length}.
+        The prize is {web3.utils.fromWei(this.state.balance)} ether</p>
+        <hr/>
+
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck? </h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input 
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          
+          </div>
+          <button>Enter</button>
+        </form>
+        <hr />
+
+        <h4>Ready to pick a winner? </h4>
+        <button onClick={this.onClick}>Pick a winner!</button>
+
+        <hr />
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
